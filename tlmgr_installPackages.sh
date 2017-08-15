2b4da23877fb45e93110997188e847458545fb3b
Dominik Gierlach <dominik.gierlach@gmai.com>
2017-08-15 11:25:22 +0200

    added script to install required tlmgr packages for termux

---
+++ tlmgr_installPackages.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+tlmgr install dejavu amsmath helvetic tex4ht cslatex csplain moderncv \
+	placeins lipsum lastpage multirow tabu varwidth background \
+	needspace enumitem framed wrapfig import biblatex logreq xstring siunitx circuitikz babel-german \
+	collection-latex collection-fontsrecommended isodate substr was 
