7e917cba78ca89b6a8f42f6b67729dc772e02092
Dominik Gierlach <dominik.gierlach@gmail.com>
2017-09-27 20:34:10 +0200

    fixed touchpad settings

--- .scripts/configTouchpad.sh
+++ .scripts/configTouchpad.sh
@@ -2,17 +2,12 @@
 
 set -eu
 
-id=$(xinput list | sed -n '/SynPS\/2 Synaptics TouchPad/s/.*id=\([0-9]\+\).*/\1/p')
+id=$(xinput list | sed -n '/AlpsPS\/2 ALPS GlidePoint/s/.*id=\([0-9]\+\).*/\1/p')
 
+palmDetection=$(xinput list-props "$id" | sed -n '/Synaptics Palm Detection (/s/.*(\([0-9]\+\)).*/\1/p')
 
-naturalScroll=$(xinput list-props "$id" | sed -n '/Natural Scrolling Enabled (/s/.*(\([0-9]\+\)).*/\1/p')
+speed=$(xinput list-props "$id" | sed -n '/Synaptics Move Speed (/s/.*(\([0-9]\+\)).*/\1/p')
 
-click=$(xinput list-props "$id" | sed -n '/libinput Click Method Enabled (/s/.*(\([0-9]\+\)).*/\1/p')
-
-tap=$(xinput list-props "$id" | sed -n '/libinput Tapping Enabled (/s/.*(\([0-9]\+\)).*/\1/p')
-
-
-xinput set-int-prop "$id" "$naturalScroll" 8 1
-xinput set-prop "$id" "$click" 0 1
-xinput set-prop "$id" "$tap" 1
+xinput set-prop "$id" "$palmDetection" 1
+xinput set-prop "$id" "$speed" 2.0 133.0 0.1 0.0 
 
