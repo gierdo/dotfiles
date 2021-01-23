d31b363ba65dcd58ced45f5abeae2abc759627c7
Dominikus Gierlach <dominik.gierlach@gmail.com>
2021-01-23 20:39:58 +0100

    switch from zsh to fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+set -gx SHELL fish
+
+if type -q pyenv
+  source (pyenv init - | source)
+end
+
+if type -q thefuck
+  thefuck --alias | source
+end
+
+if test -f "$HOME/.dotfiles/asdf/completions/asdf.fish"
+  source "$HOME/.dotfiles/asdf/completions/asdf.fish"
+end
+
+if type -q nvim
+  alias vim=nvim
+end
+
+if type -q kubectl
+  alias kubectl-show-ns='kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found -n'
+end
+
+if test -f "$HOME/.config.fish.local"
+  source "$HOME/.config.fish.local"
+end
