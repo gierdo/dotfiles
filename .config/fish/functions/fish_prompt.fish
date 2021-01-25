e4d70bc7154c77da0041042c480beaaf05e50cb6
Dominikus Gierlach <dominik.gierlach@gmail.com>
2021-01-25 20:13:29 +0100

    make fish virtualenv-aware

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -28,6 +28,10 @@ function fish_prompt --description 'Write out the prompt'
         set_color $fish_color_error
     end
 
+    if set -q VIRTUAL_ENV
+      echo -n -s (set_color -b cyan white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+    end
+
     echo -n '➤ '
     set_color normal
 end
