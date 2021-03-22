e15e074d294a20ed4cae146a467a8df1e1692fa6
Dominikus Gierlach <dominik.gierlach@gmail.com>
2021-03-22 11:50:01 +0100

    fzf fish integration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ set -gx SHELL (which fish)
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
 fundle plugin 'evanlucas/fish-kubectl-completions'
+fundle plugin 'PatrickF1/fzf.fish'
 
 fundle init
 
