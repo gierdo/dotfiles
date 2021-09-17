7fbcb0d8a84cf35f97aa00d56d6a9d1d275dbb25
Dominikus Gierlach <dominik.gierlach@gmail.com>
2021-09-17 11:26:47 +0200

    remove i3lock from the i3 profile

--- guix-manifests/i3-profile.scm
+++ guix-manifests/i3-profile.scm
@@ -1,8 +1,9 @@
 (specifications->manifest
   '("i3-wm"
-    "i3lock"
     "ungoogled-chromium"
     "i3status"
     "xwallpaper"
     "rofi"
     "autorandr"))
+
+; i3lock from guix doesn't cooperate with pam on ubuntu..
