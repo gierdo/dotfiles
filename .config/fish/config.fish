19882c116b598d8ae440468c8b9fc338d63878c6
Dominikus Gierlach <dominik.gierlach@gmail.com>
2021-10-07 10:41:00 +0200

    use guix fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
 fundle plugin 'evanlucas/fish-kubectl-completions'
 fundle plugin 'sentriz/fish-pipenv'
+fundle plugin 'PatrickF1/fzf.fish'
 
 fundle init
 
