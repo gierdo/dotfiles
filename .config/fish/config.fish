9442d978c761bfe27eb5312bd83cd22cc5c12bc7
Dominikus Gierlach <dominik.gierlach@gmail.com>
2021-11-08 10:37:49 +0100

    remove fish-pipenv plugin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,6 @@ set -gx SHELL (command -v fish)
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
 fundle plugin 'evanlucas/fish-kubectl-completions'
-fundle plugin 'sentriz/fish-pipenv'
 fundle plugin 'PatrickF1/fzf.fish'
 
 fundle init
