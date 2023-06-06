3cce076e77cfe1b2fc9b7e870b1ebb66abf60980
Dominikus Gierlach <dominik.gierlach@gmail.com>
2023-06-06 10:57:15 +0200

    restructure ranger

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -1,27 +1,20 @@
-# This is a sample commands.py.  You can add your own commands here.
-#
 # Please refer to commands_full.py for all the default commands and a complete
 # documentation.  Do NOT add them all here, or you may end up with defunct
 # commands when upgrading ranger.
-
-# A simple command for demonstration purposes follows.
-# -----------------------------------------------------------------------------
 
 from __future__ import absolute_import, division, print_function
 
 # You can import any python module as needed.
 import sys
-
 from pathlib import Path
 
 # Allow adding plugins from the dotfiles directory
-sys.path.append(str(Path.home()) + "/.dotfiles/ranger/plugins")
+sys.path.append(str(Path.home()) + "/.dotfiles/.config/ranger/plugins")
+
+from ranger_udisk_menu.mounter import mount
 
 # You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
-
-
-from ranger_udisk_menu.mounter import mount
 
 
 class fzf_select(Command):
@@ -36,8 +29,8 @@ class fzf_select(Command):
     """
 
     def execute(self):
-        import subprocess
         import os.path
+        import subprocess
 
         if self.quantifier:
             # match only directories
