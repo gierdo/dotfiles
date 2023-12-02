80b70b6019b97013ed8780bcf9da42d73554db54
Dominikus Gierlach <dominik.gierlach@gmail.com>
2023-12-02 18:09:06 +0100

    lualine global status

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -65,7 +65,7 @@ return {
           },
           ignore_focus = {},
           always_divide_middle = true,
-          globalstatus = false,
+          globalstatus = true,
           refresh = {
             statusline = 1000,
             tabline = 1000,
