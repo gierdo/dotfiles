c4e69490e8d8f0394460f3e20a678973fd256c40
Dominikus Gierlach <dominik.gierlach@gmail.com>
2023-12-06 11:43:21 +0100

    pynvim virtualenv

--- .config/nvim/lua/config_d/python_env.lua
+++ .config/nvim/lua/config_d/python_env.lua
@@ -1,11 +1,28 @@
 -- Python bindings break in virtual environments with pyenv etc.
 --
 -- With path resolution of the python executable, pynvim is not installed in
--- the active environment anymore if a virtual env is active. Solution: Set the
--- "global" python executable as python host prog
+-- the active environment anymore if a virtual env is active.
+-- Solution: Create and use a dedicated venv for nvim with pyenv
 
-local home = os.getenv("HOME")
-local pythonpath = home .. "/.dotfiles/pyenv/shims/python"
+
+local function directory_exists(name)
+  if type(name) ~= "string" then return false end
+  return os.rename(name, name) and true or false
+end
+
+local pyenv_root = os.getenv("PYENV_ROOT")
+local nvim_virtualenv_root = pyenv_root .. "/versions/nvim"
+
+-- Set up default virtualenv for nvim
+if not directory_exists(nvim_virtualenv_root) then
+  os.execute [[
+  pyenv virtualenv --force nvim
+  PYENV_VERSION=nvim pip install pynvim
+  ]]
+end
+
+local pythonpath = nvim_virtualenv_root .. "/bin/python"
+
 
 vim.g.python3_host_prog = pythonpath
 vim.g.python_host_prog = pythonpath
