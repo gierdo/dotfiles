3adae99908c61e28eb61365600c944aa257e412a
Dominikus Gierlach <dominik.gierlach@gmail.com>
2023-12-14 13:25:21 +0100

    fix diagnostic highlighting with theme inheritance

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       vim.o.background = 'dark' -- or 'light'
       require('solarized').setup({
-        transparent = false,    -- enable transparent background
+        transparent = false,
         styles = {
           comments = {},
           functions = {},
@@ -22,7 +22,7 @@ return {
           cmp = true,
           diagnostic = true,
           dashboard = true,
-          editor = false,
+          editor = true,
           gitsign = true,
           hop = true,
           indentblankline = true,
@@ -35,107 +35,15 @@ return {
           semantic = true,
           syntax = true,
           telescope = true,
-          tree = false,
+          tree = true,
           treesitter = true,
           whichkey = true,
           mini = true,
         },
         highlights = function(colors, colorhelper)
           return {
-            -- nvim-tree configuration
-            NvimTreeSymlink = { link = 'Underlined' },
-            NvimTreeSymlinkIcon = { link = 'Directory' },
-            NvimTreeFolderName = { fg = colors.base0 }, -- (Directory)
-            NvimTreeRootFolder = { link = 'Title' },
-            NvimTreeFolderIcon = { link = 'Directory' },
-            NvimTreeEmptyFolderName = { fg = colors.base0 }, -- (Directory)
-            NvimTreeExecFile = { link = 'Function' },
-            NvimTreeOpenedFile = { fg = colors.blue, bold = true },
-            NvimTreeModifiedFile = { fg = colors.change },
-            NvimTreeSpecialFile = { link = 'Special' },
-            NvimTreeIndentMarker = { fg = colors.base01 },
-            NvimTreeLspDiagnosticsInformation = {}, -- (DiagnosticInfo)
-            NvimTreeLspDiagnosticsHint = {},        -- (DiagnosticHint)
-            NvimTreeGitDirty = { fg = colors.change },
-            NvimTreeGitStaged = { fg = colors.add },
-            NvimTreeGitMerge = { fg = colors.change },
-            NvimTreeGitRenamed = { fg = colors.add },
-            NvimTreeGitNew = { fg = colors.add },
-            NvimTreeGitDeleted = { fg = colors.delete },
-            NvimTreeNormal = { fg = colors.base0, bg = colors.base04 },
-            NvimTreeNormalFloat = { link = 'NvimTreeNormal' },
-            NvimTreeEndOfBuffer = { fg = colors.base04 }, -- (NonText)
-
-            -- general editor configuration
-            ColorColumn = { bg = colors.base02 },                                                         -- used for columns
-            Conceal = { fg = colors.blue },                                                               -- placeholder characters
-            CurSearch = { fg = colors.base2, bg = colors.base02 },                                        -- highlight under cursor
-            Cursor = { fg = colors.base03, bg = colors.base0 },                                           -- character under cursor
-            lCursor = { link = 'Cursor' },                                                                -- character under the cursor
-            CursorIM = { link = 'Cursor' },                                                               -- like cursor, but IME mode
-            CursorColumn = { link = 'ColorColumn' },                                                      -- screen-column at the cursor
-            CursorLine = { bg = colors.base02, sp = colors.base1 },                                       -- screen-line at the cursor
-            Directory = { fg = colors.blue },                                                             -- directory names
-            DiffAdd = { fg = colors.add, reverse = true },                                                -- Added line
-            DiffChange = { fg = colors.change, reverse = true },                                          -- Changed line
-            DiffDelete = { fg = colors.delete, reverse = true },                                          -- Deleted line
-            DiffText = { fg = colors.blue, reverse = true },                                              -- Changed Text
-            EndOfBuffer = { fg = colors.base03 },                                                         -- Filler lines (~)
-            TermCursor = { link = 'Cursor' },                                                             -- Cursor in a focused terminal
-            TermCursorNC = { fg = colors.base03, bg = colors.base0 },                                     -- Cursor in an unfocused terminal
-            ErrorMsg = { fg = colors.error, reverse = true },                                             -- Error messages on the command line
-            WinSeparator = { fg = colors.base00, bg = colors.base02 },
-            Folded = { fg = colors.base0, bg = colors.base02, underline = true, bold = true },            -- Line used for closed folds
-            FoldColumn = { fg = colors.base0, bg = colors.base02, bold = true },                          -- 'foldcolumn'
-            SignColumn = { fg = colors.base0, bg = colors.base02 },
-            IncSearch = { fg = colors.orange, standout = true },                                          -- 'incsearch' highlighting, also for the text replaced
-            Substitute = { link = 'IncSearch' },                                                          -- :substitute replacement text highlight
-            LineNr = { fg = colors.base01, bg = colors.base02 },
-            LineNrAbove = { link = 'LineNr' },                                                            -- Line number, above the cursor line
-            LineNrBelow = { link = 'LineNr' },                                                            -- Line number, below the cursor
-            CursorLineNr = { fg = colors.base1, bg = colors.base02, bold = true },
-            CursorLineFold = { link = 'FoldColumn' },                                                     -- Like FoldColumn when 'cursorline' is set
-            CursorLineSign = { link = 'SignColumn' },                                                     -- Like SignColumn when 'cursorline' is set
-            MatchParen = { fg = colors.red, bg = colors.base01, bold = true },                            -- Character under the cursor or just before it
-            ModeMsg = { fg = colors.blue },                                                               -- 'showmode' message (e.g., "-- INSERT --")
-            MsgArea = { link = 'Normal' },                                                                -- 'Area for messages and cmdline'
-            MsgSeparator = { link = 'Normal' },                                                           -- Separator for scrolled messages msgsep.
-            MoreMsg = { fg = colors.blue },                                                               -- more-prompt
-            NonText = { fg = colors.base1, bold = true },                                                 -- '@' at the end of the window
-            Normal = { fg = colors.base0, bg = colors.base03 },
-            NormalFloat = { fg = colors.base0, bg = colors.base02 },                                      -- Normal text in floating windows
-            FloatBorder = { link = 'WinSeparator', bold = true },                                         -- Border of floating windows.
-            FloatTitle = { fg = colors.orange },                                                          -- Title of float windows.
-            NormalNC = { link = 'Normal' },                                                               -- Normal text in non-current windows.
-            Pmenu = { fg = colors.base0, bg = colors.base02 },
-            PmenuSel = { fg = colors.base2, bg = colors.base01 },                                         -- Popup menu: Selected item
-            PmenuKind = { link = 'Pmenu' },                                                               -- Popup menu: Normal item kind
-            PmenuKindSel = { link = 'PmenuSel' },                                                         -- Popup menu: Selected item kind
-            PmenuExtra = { link = 'Pmenu' },                                                              -- Popup menu: Normal item extra text
-            PmenuExtraSel = { link = 'PmenuSel' },                                                        -- Popup menu: Selected item extra text
-            PmenuSbar = { fg = colors.base0, bg = colors.base2 },                                         -- Popup menu: Scrollbar
-            PmenuThumb = { fg = colors.base03, bg = colors.base0 },                                       -- Popup menu: Thumb of the scrollbar
-            Question = { fg = colors.cyan, bold = true },                                                 -- hit-enter prompt and yes/no questions.
-            QuickFixLine = { fg = colors.base0, bg = colors.base03 },                                     -- Current quickfix item in the quickfix window
-            Search = { fg = colors.yellow, reverse = true },                                              -- Last search pattern highlighting
-            SpecialKey = { fg = colors.red, reverse = true },                                             -- Unprintable characters: Text displayed differently from what it really is.
-            SpellBad = { sp = colors.red, undercurl = true },                                             -- Word that is not recognized by the spellchecker.
-            SpellCap = { sp = colors.violet, undercurl = true },                                          -- Word that should start with a capital
-            SpellLocal = { sp = colors.yellow, undercurl = true },                                        -- Word that is recognized by the spellchecker as one that is used in another region
-            SpellRare = { sp = colors.cyan, undercurl = true },                                           -- Word that is recognized by the spellchecker as one that is hardly ever used.
-            StatusLine = { fg = colors.base02, bg = colors.base1 },                                       -- Status line of current window.
-            StatusLineNC = { fg = colors.base02, bg = colors.base00 },                                    -- Status lines of not-current windows.
-            TabLine = { fg = colors.base0, bg = colors.base02, sp = colors.base0, underline = true },     -- Tab pages line, not active tab page label.
-            TabLineFill = { fg = colors.base0, bg = colors.base02, sp = colors.base0, underline = true }, -- Tab pages line, where there are no labels.
-            TabLineSel = { fg = colors.base2, bg = colors.base01, sp = colors.base0, underline = true },  -- Tab pages line, active tab page label.
-            Title = { fg = colors.orange, bold = true },                                                  -- Titles for output from ":set all", ":autocmd" etcolors.
-            Visual = { bg = colors.base02, standout = true },                                             -- Visual mode selection.
-            VisualNOS = { link = 'Visual' },                                                              -- Visual mode selection when vim is "Not Owning the Selection".
-            WarningMsg = { fg = colors.warning, bold = true },                                            -- Warning messages.
-            Whitespace = { fg = colors.base01 },                                                          -- "nbsp", "space", "tab", "multispace", "lead" and "trail" in 'listchars'.
-            WildMenu = { fg = colors.base2, bg = colors.base02 },                                         -- Current match in 'wildmenu' completion.
-            WinBar = { link = 'Pmenu' },                                                                  -- Window bar of current window.
-            WinBarNC = { link = 'WinBar' },                                                               -- Window bar of not-current windows.
+            NvimTreeWinSeparator = { link = 'WinSeparator' },     -- Use normal window separator lines for nvim tree
+            CocFadeOut = { fg = colors.info, bg = colors.base1 }, -- Change background of diagnostic-info-sections
           }
         end,
         colors = {},
