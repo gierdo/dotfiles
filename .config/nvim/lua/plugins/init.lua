d9084a47b6b96b50900efdfda8326f17ed95ef53
Dominikus Gierlach <dominik.gierlach@gmail.com>
2024-02-12 08:17:53 +0100

    de-annoy markdown

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -122,6 +122,8 @@ return {
 
 		config = function()
 			vim.g.vim_markdown_folding_disabled = 1
+			vim.g.vim_markdown_auto_insert_bullets = 0
+			vim.g.vim_markdown_new_list_item_indent = 0
 		end
 	},
 	{
