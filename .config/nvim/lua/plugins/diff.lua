7b0ee5ab669a6b087cd67a59750b5333ca02338c
Dominikus Gierlach <dominik.gierlach@gmail.com>
2024-03-29 14:46:20 +0100

    Added Neogit and others

--- .config/nvim/lua/plugins/diff.lua
+++ .config/nvim/lua/plugins/diff.lua
@@ -1,8 +1,19 @@
 return {
   {
     'sindrets/diffview.nvim',
+    event = 'VeryLazy',
     config = function()
       require("diffview").setup()
     end,
   },
+  {
+    "NeogitOrg/neogit",
+    event = 'VeryLazy',
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true
+  }
 }
