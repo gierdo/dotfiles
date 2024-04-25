05d8d517b2590181105fe7e7bd74f7b6d7fccefe
Dominikus Gierlach <dominik.gierlach@gmail.com>
2024-04-25 09:08:23 +0200

    Use llama3 instruct

--- gists/install-llama-models.sh
+++ gists/install-llama-models.sh
@@ -17,4 +17,4 @@ function install_model() {
 }
 
 install_model https://huggingface.co/bartowski/dolphin-2.8-mistral-7b-v02-GGUF/resolve/main/dolphin-2.8-mistral-7b-v02-Q4_K_M.gguf dolphin-2.8-mistral-7b-v02-Q4_K_M.gguf
-install_model https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_K_M.gguf mistral-7b-instruct-v0.2.Q4_K_M.gguf
+install_model https://huggingface.co/QuantFactory/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct.Q4_K_M.gguf Meta-Llama-3-8B-Instruct.Q4_K_M.gguf
