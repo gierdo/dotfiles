add584ef415762c29044a7bd720fefcf33150abc
Dominikus Gierlach <dominik.gierlach@gmail.com>
2024-05-29 08:03:01 +0200

    try out community codellama 3

--- gists/install-llama-models.sh
+++ gists/install-llama-models.sh
@@ -16,5 +16,5 @@ function install_model() {
   fi
 }
 
-install_model https://huggingface.co/bartowski/dolphin-2.8-mistral-7b-v02-GGUF/resolve/main/dolphin-2.8-mistral-7b-v02-Q4_K_M.gguf dolphin-2.8-mistral-7b-v02-Q4_K_M.gguf
+install_model https://huggingface.co/bartowski/Llama-3-8B-Instruct-Coder-GGUF/resolve/main/Llama-3-8B-Instruct-Coder-Q4_K_M.gguf Llama-3-8B-Instruct-Coder-Q4_K_M.gguf
 install_model https://huggingface.co/QuantFactory/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct.Q4_K_M.gguf Meta-Llama-3-8B-Instruct.Q4_K_M.gguf
