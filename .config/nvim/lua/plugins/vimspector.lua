f0890060e57bd78ee41c7599f8d718b9334d918e
Dominikus Gierlach <dominikus.gierlach@man.eu>
2024-07-15 15:38:23 +0200

    adapt to new which-key spec

--- .config/nvim/lua/plugins/vimspector.lua
+++ .config/nvim/lua/plugins/vimspector.lua
@@ -8,31 +8,49 @@ return {
       vim.g.vimspector_terminal_minwidth = 75
       local wk = require("which-key")
 
-      wk.register {
-        ["<leader>d"] = {
-          name = "  Debug",
-          c = { "<Plug>VimspectorContinue<cr>", "When debugging, continue. Otherwise start debugging." },
-          s = { "<Plug>VimspectorStop<cr>", "Stop debugging." },
-          re = { "<Plug>VimpectorRestart<cr>", "Restart debugging with the same configuration." },
-          p = { "<Plug>VimspectorPause<cr>", "Pause debuggee." },
-          t = { "<Plug>VimspectorToggleBreakpoint<cr>", "Toggle line breakpoint on the current line." },
-          bs = { "<Plug>VimspectorBreakpoints<cr>", "Show/hide the breakpoints window." },
-          bn = { "<Plug>VimspectorJumpToNextBreakpoint<cr>", "Move Cursor to the next breakpoint in current file." },
-          bp = { "<Plug>VimspectorJumpToPreviousBreakpoint<cr>", "Move Cursor to the previous breakpoint in current file." },
-          btc = { "<Plug>VimspectorToggleConditionalBreakpoint<cr>", "Toggle conditional line breakpoint or logpoint on the current line." },
-          bf = { "<Plug>VimspectorAddFunctionBreakpoint<cr>", "Add a function breakpoint for the expression under cursor." },
-          r = { "<Plug>VimspectorRunToCursor<cr>", "Run to Cursor." },
-          d = { "<Plug>VimspectorDisassemble<cr>", "Show disassembly. Enable instruction stepping." },
-          fu = { "<Plug>VimspectorUpFrame<cr>", "Move up a frame in the current call stack." },
-          fd = { "<Plug>VimspectorDownFrame<cr>", "Move down a frame in the current call stack." },
-          i = { "<Plug>VimspectorBalloonEval<cr>", "Evaluate expression under cursor (or visual) in popup." },
-          -- { "<Plug>VimspectorGoToCurrentLine<cr>",             "Reset the current program counter to the current line." },
-          -- { "<Plug>VimspectorStepOver<cr>",                    "Step Over." },
-          -- { "<Plug>VimspectorStepInto<cr>",                    "Step Into." },
-          -- { "<Plug>VimspectorStepOut<cr>",                     "Step out of current function scope." },
-          -- { "<Plug>VimspectorJumpToProgramCounter<cr>",        "Move Cursor to the program counter in the current frame." },
+      wk.add({
+        { "<leader>d", group = "  Debug" },
+        {
+          "<leader>dbf",
+          "<Plug>VimspectorAddFunctionBreakpoint<cr>",
+          desc =
+          "Add a function breakpoint for the expression under cursor."
         },
-      }
+        {
+          "<leader>dbn",
+          "<Plug>VimspectorJumpToNextBreakpoint<cr>",
+          desc =
+          "Move Cursor to the next breakpoint in current file."
+        },
+        {
+          "<leader>dbp",
+          "<Plug>VimspectorJumpToPreviousBreakpoint<cr>",
+          desc =
+          "Move Cursor to the previous breakpoint in current file."
+        },
+        { "<leader>dbs", "<Plug>VimspectorBreakpoints<cr>", desc = "Show/hide the breakpoints window." },
+        {
+          "<leader>dbtc",
+          "<Plug>VimspectorToggleConditionalBreakpoint<cr>",
+          desc =
+          "Toggle conditional line breakpoint or logpoint on the current line."
+        },
+        { "<leader>dc",  "<Plug>VimspectorContinue<cr>",    desc = "When debugging, continue. Otherwise start debugging." },
+        { "<leader>dd",  "<Plug>VimspectorDisassemble<cr>", desc = "Show disassembly. Enable instruction stepping." },
+        { "<leader>dfd", "<Plug>VimspectorDownFrame<cr>",   desc = "Move down a frame in the current call stack." },
+        { "<leader>dfu", "<Plug>VimspectorUpFrame<cr>",     desc = "Move up a frame in the current call stack." },
+        {
+          "<leader>di",
+          "<Plug>VimspectorBalloonEval<cr>",
+          desc =
+          "Evaluate expression under cursor (or visual) in popup."
+        },
+        { "<leader>dp",  "<Plug>VimspectorPause<cr>",            desc = "Pause debuggee." },
+        { "<leader>dr",  "<Plug>VimspectorRunToCursor<cr>",      desc = "Run to Cursor." },
+        { "<leader>dre", "<Plug>VimpectorRestart<cr>",           desc = "Restart debugging with the same configuration." },
+        { "<leader>ds",  "<Plug>VimspectorStop<cr>",             desc = "Stop debugging." },
+        { "<leader>dt",  "<Plug>VimspectorToggleBreakpoint<cr>", desc = "Toggle line breakpoint on the current line." },
+      })
     end,
     dependencies = {
       "folke/which-key.nvim",
