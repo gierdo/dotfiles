0b2259fd3504076f5c9b2164f0553184e9d60345
Dominikus Gierlach <dominikus.gierlach@man.eu>
2024-07-28 10:12:45 +0200

    gpgme also on ubuntu

--- gists/setup-gpgme-json.sh
+++ gists/setup-gpgme-json.sh
@@ -6,29 +6,31 @@ REPO_DIRECTORY=$REPO_BASE/$REPO_NAME
 
 RELEASE_TAG=gpgme-1.23.2
 
-if [ -d $REPO_DIRECTORY ]; then
-  echo "repo already exists"
-else
-  mkdir -p $REPO_BASE
-  cd $REPO_BASE || return
-  git clone git://git.gnupg.org/gpgme.git $REPO_NAME
+if ! command -v gpgme-json &> /dev/null; then
+  if [ -d $REPO_DIRECTORY ]; then
+    echo "repo already exists"
+  else
+    mkdir -p $REPO_BASE
+    cd $REPO_BASE || return
+    git clone git://git.gnupg.org/gpgme.git $REPO_NAME
+  fi
+
+  cd $REPO_DIRECTORY || return
+
+  git checkout $RELEASE_TAG
+  ./autogen.sh
+  mkdir -p build
+  cd build || return
+  ../configure --prefix="$HOME/.local"
+  make install
 fi
-
-cd $REPO_DIRECTORY || return
-
-git checkout $RELEASE_TAG
-./autogen.sh
-mkdir -p build
-cd build || return
-../configure --prefix="$HOME/.local"
-make install
 
 mkdir -p ~/.mozilla/native-messaging-hosts
 cat >~/.mozilla/native-messaging-hosts/gpgmejson.json <<EOF
 {
   "name": "gpgmejson",
   "description": "JavaScript binding for GnuPG",
-  "path": "$HOME/.local/bin/gpgme-json",
+  "path": "$(which gpgme-json)",
   "type": "stdio",
   "allowed_extensions": [
   "jid1-AQqSMBYb0a8ADg@jetpack"
@@ -41,10 +43,23 @@ cat >~/.config/google-chrome/NativeMessagingHosts/gpgmejson.json <<EOF
 {
   "name": "gpgmejson",
   "description": "JavaScript binding for GnuPG",
-  "path": "$HOME/.local/bin/gpgme-json",
+  "path": "$(which gpgme-json)",
   "type": "stdio",
   "allowed_origins": [
   "chrome-extension://kajibbejlbohfaggdiogboambcijhkke/"
+  ]
+}
+EOF
+
+mkdir -p ~/.config/microsoft-edge/NativeMessagingHosts
+cat >~/.config/microsoft-edge/NativeMessagingHosts/gpgmejson.json <<EOF
+{
+  "name": "gpgmejson",
+  "description": "JavaScript binding for GnuPG",
+  "path": "$(which gpgme-json)",
+  "type": "stdio",
+  "allowed_origins": [
+  "chrome-extension://dgcbddhdhjppfdfjpciagmmibadmoapc/"
   ]
 }
 EOF
