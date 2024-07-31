3a1a6e79a8b37cbd703f834caa9271ec3d597d6d
Dominikus Gierlach <dominikus.gierlach@man.eu>
2024-07-31 13:42:02 +0200

    integrate llama 3.1

--- gists/install-llama-models.sh
+++ gists/install-llama-models.sh
@@ -16,5 +16,6 @@ function install_model() {
   fi
 }
 
+install_model https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF/resolve/main/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf
 install_model https://huggingface.co/bartowski/Llama-3-8B-Instruct-Coder-GGUF/resolve/main/Llama-3-8B-Instruct-Coder-Q4_K_M.gguf Llama-3-8B-Instruct-Coder-Q4_K_M.gguf
 install_model https://huggingface.co/lmstudio-community/Llama-3-Groq-8B-Tool-Use-GGUF/resolve/main/Llama-3-Groq-8B-Tool-Use-Q4_K_M.gguf Llama-3-Groq-8B-Tool-Use-Q4_K_M.gguf
