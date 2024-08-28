8f527ce25e55750ede20f76338ab1e0fe675f135
Dominikus Gierlach <dominikus.gierlach@man.eu>
2024-08-28 09:38:13 +0200

    disable nvim transparency explicitly

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -6,6 +6,9 @@ return {
     config = function()
       vim.o.background = 'dark' -- or 'light'
       require('solarized').setup({
+        transparent = {
+          enabled = false
+        },
         styles = {
           comments = {},
           functions = {},
