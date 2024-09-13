1b455408c2dbce2eb3365e836e2565ca44ebc312
Dominikus Gierlach <dominik.gierlach@gmail.com>
2024-09-13 18:34:57 +0200

    remove coc remnants

--- .config/nvim/lua/plugins/fuzzyness.lua
+++ .config/nvim/lua/plugins/fuzzyness.lua
@@ -1,27 +1,27 @@
 local utils = require("utils")
 return {
 	{
-		'junegunn/fzf',
+		"junegunn/fzf",
 		lazy = true,
-		build = './install --all'
+		build = "./install --all",
 	},
 	{
-		'gelguy/wilder.nvim',
+		"gelguy/wilder.nvim",
 		event = "VeryLazy",
 		build = function()
 			vim.cmd("UpdateRemotePlugins")
 		end,
 		config = function()
-			local wilder = require('wilder')
+			local wilder = require("wilder")
 			wilder.setup({
-				modes = { ':' },
-				enable_cmdline_enter = 0
+				modes = { ":" },
+				enable_cmdline_enter = 0,
 			})
-			wilder.set_option('pipeline', {
+			wilder.set_option("pipeline", {
 				wilder.branch(
 					wilder.cmdline_pipeline({
 						-- sets the language to use, 'vim' and 'python' are supported
-						language = 'python',
+						language = "python",
 						-- 0 turns off fuzzy matching
 						-- 1 turns on fuzzy matching
 						-- 2 partial fuzzy matching (match does not have to begin with the same first letter)
@@ -34,43 +34,42 @@ return {
 						sorter = wilder.python_difflib_sorter(),
 						-- can be set to 're2' for performance, requires pyre2 to be installed
 						-- see :h wilder#python_search() for more details
-						engine = 're2',
+						engine = "re2",
 					})
 				),
 			})
-			wilder.set_option('renderer', wilder.popupmenu_renderer({
-				-- highlighter applies highlighting to the candidates
-				highlighter = wilder.basic_highlighter(),
-			}))
+			wilder.set_option(
+				"renderer",
+				wilder.popupmenu_renderer({
+					-- highlighter applies highlighting to the candidates
+					highlighter = wilder.basic_highlighter(),
+				})
+			)
 		end,
 	},
 	{
-		'nvim-telescope/telescope.nvim',
-		event = 'VeryLazy',
-		branch = '0.1.x',
+		"nvim-telescope/telescope.nvim",
+		event = "VeryLazy",
+		branch = "0.1.x",
 		init = function()
-			vim.keymap.set('n', '<A-l>', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
-			vim.keymap.set('n', '<C-A-l>', '<cmd>Telescope grep_string search=""<cr>')
-			vim.keymap.set('n', '<A-p>', '<cmd>Telescope live_grep<cr>')
-			vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files<cr>')
-			vim.keymap.set('n', '<C-A-b>', '<cmd>Telescope buffers<cr>')
-			vim.keymap.set('n', '<C-A-p>', '<cmd>Telescope tags<cr>')
+			vim.keymap.set("n", "<A-l>", "<cmd>Telescope current_buffer_fuzzy_find<cr>")
+			vim.keymap.set("n", "<C-A-l>", '<cmd>Telescope grep_string search=""<cr>')
+			vim.keymap.set("n", "<A-p>", "<cmd>Telescope live_grep<cr>")
+			vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>")
+			vim.keymap.set("n", "<C-A-b>", "<cmd>Telescope buffers<cr>")
+			vim.keymap.set("n", "<C-A-p>", "<cmd>Telescope tags<cr>")
 		end,
 		config = function()
-			local actions = require('telescope.actions')
+			local actions = require("telescope.actions")
 			local action_state = require("telescope.actions.state")
 
 			local custom_actions = {}
 
-			require('telescope').load_extension('coc')
-			require('telescope').load_extension('gh')
-			require('telescope').load_extension('fzf')
-			require('telescope').load_extension('vimspector')
+			require("telescope").load_extension("gh")
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("vimspector")
 
-			local builtin = require('telescope.builtin')
-			local coc_builtin = require('telescope').extensions.coc
-
-			builtin.coc = coc_builtin.coc
+			local builtin = require("telescope.builtin")
 
 			function custom_actions.fzf_multi_select(prompt_bufnr)
 				local picker = action_state.get_current_picker(prompt_bufnr)
@@ -86,7 +85,7 @@ return {
 				end
 			end
 
-			require('telescope').setup {
+			require("telescope").setup({
 				defaults = {
 					-- Default configuration for telescope goes here:
 					-- config_key = value,
@@ -98,14 +97,14 @@ return {
 							["<esc>"] = actions.close,
 							["<tab>"] = actions.toggle_selection + actions.move_selection_next,
 							["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
-							["<cr>"] = custom_actions.fzf_multi_select
+							["<cr>"] = custom_actions.fzf_multi_select,
 						},
 						n = {
 							["<tab>"] = actions.toggle_selection + actions.move_selection_next,
 							["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
-							["<cr>"] = custom_actions.fzf_multi_select
-						}
-					}
+							["<cr>"] = custom_actions.fzf_multi_select,
+						},
+					},
 				},
 				pickers = {
 					-- Default configuration for builtin pickers goes here:
@@ -118,9 +117,7 @@ return {
 					find_files = {
 						hidden = true,
 					},
-					lsp_references = {
-
-					},
+					lsp_references = {},
 				},
 				extensions = {
 					-- Your extension configuration goes here:
@@ -128,35 +125,34 @@ return {
 					--   extension_config_key = value,
 					-- }
 					-- please take a look at the readme of the extension you want to configure
-				}
-			}
+				},
+			})
 		end,
 		dependencies = {
-			'nvim-lua/plenary.nvim',
-			'fannheyward/telescope-coc.nvim',
-			'nvim-telescope/telescope-github.nvim',
-			'nvim-telescope/telescope-vimspector.nvim',
-			'nvim-telescope/telescope-fzf-native.nvim',
-		}
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-github.nvim",
+			"nvim-telescope/telescope-vimspector.nvim",
+			"nvim-telescope/telescope-fzf-native.nvim",
+		},
 	},
 	{
-		'nvim-telescope/telescope-fzf-native.nvim',
+		"nvim-telescope/telescope-fzf-native.nvim",
 		lazy = true,
-		build = 'make',
-		dependencies = { 'junegunn/fzf' }
+		build = "make",
+		dependencies = { "junegunn/fzf" },
 	},
-	{ 'fannheyward/telescope-coc.nvim',           lazy = true },
-	{ 'nvim-telescope/telescope-github.nvim',     lazy = true },
-	{ 'nvim-telescope/telescope-vimspector.nvim', lazy = true },
+	{ "fannheyward/telescope-coc.nvim", lazy = true },
+	{ "nvim-telescope/telescope-github.nvim", lazy = true },
+	{ "nvim-telescope/telescope-vimspector.nvim", lazy = true },
 	{
-		'nvim-pack/nvim-spectre',
+		"nvim-pack/nvim-spectre",
 		event = "VeryLazy",
 		config = function()
-			require('spectre').setup()
+			require("spectre").setup()
 			vim.cmd.command("Replace", "Spectre")
 		end,
 		dependencies = {
-			'nvim-lua/plenary.nvim'
-		}
+			"nvim-lua/plenary.nvim",
+		},
 	},
 }
