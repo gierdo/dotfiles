ece6dd6fae66416f4d8c798e4c228239419cdbd5
Dominikus Gierlach <dominikus.gierlach@man.eu>
2024-09-13 19:51:52 +0200

    use telescope for lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -16,7 +16,8 @@ return {
 		config = function()
 			-- note: diagnostics are not exclusive to lsp servers
 			-- so these can be global keybindings
-			vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+			vim.keymap.set("n", "gl", "<cmd>lua require('telescope.builtin').diagnostics({bufnr=0})<cr>") -- Open diagnostics of current buffer
+			vim.keymap.set("n", "gL", "<cmd>lua require('telescope.builtin').diagnostics()<cr>") -- Open diagnostics of all buffers
 			vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
 			vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
 
@@ -28,16 +29,16 @@ return {
 					-- these will be buffer-local keybindings
 					-- because they only work if you have an active language server
 
-					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-					vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+					vim.keymap.set("n", "gd", "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>", opts)
+					vim.keymap.set("n", "gi", "<cmd>lua require('telescope.builtin').lsp_implementations()<cr>", opts)
+					vim.keymap.set("n", "go", "<cmd>lua require('telescope.builtin').lsp_type_definitions()<cr>", opts)
+					vim.keymap.set("n", "gr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>", opts)
 					vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-					vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-					vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-					vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
 					vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
 					vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-					vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-					vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+					vim.keymap.set({ "n", "x" }, "gf", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+					vim.keymap.set({ "n", "x" }, "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 				end,
 			})
 
@@ -138,6 +139,7 @@ return {
 			"hrsh7th/cmp-nvim-lsp-signature-help",
 			"dcampos/nvim-snippy",
 			"dcampos/cmp-snippy",
+			"nvim-telescope/telescope.nvim",
 		},
 	},
 	{
