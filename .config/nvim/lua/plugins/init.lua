bc2a0dd8fa24b46b4759396cb007f8bd79881955
Dominikus Gierlach <dominikus.gierlach@man.eu>
2024-09-20 13:35:18 +0200

    remove dev-container plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -183,36 +183,6 @@ return {
     ft = { "markdown" },
   },
   {
-    "https://codeberg.org/esensar/nvim-dev-container",
-    event = "VeryLazy",
-    config = function()
-      require("devcontainer").setup({
-        attach_mounts = {
-          neovim_config = {
-            -- enables mounting local config to /root/.config/nvim in container
-            enabled = true,
-            -- makes mount readonly in container
-            options = { "readonly" },
-          },
-          neovim_data = {
-            -- enables mounting local data to /root/.local/share/nvim in container
-            enabled = true,
-            -- no options by default
-            options = {},
-          },
-          -- Only useful if using neovim 0.8.0+
-          neovim_state = {
-            -- enables mounting local state to /root/.local/state/nvim in container
-            enabled = true,
-            -- no options by default
-            options = {},
-          },
-        },
-      })
-    end,
-    dependencies = "nvim-treesitter/nvim-treesitter",
-  },
-  {
     "glacambre/firenvim",
     lazy = not vim.g.started_by_firenvim,
     build = ":call firenvim#install(0)",
