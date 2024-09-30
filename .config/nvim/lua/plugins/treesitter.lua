b909268c39a96569f866835fc2d8060b577f744c
Dominikus Gierlach <dominik.gierlach@gmail.com>
2024-09-30 20:54:55 +0200

    clean up treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -35,118 +35,57 @@ return {
     event = "VeryLazy",
     ---@type Flash.Config
     opts = {
-      -- labels = "abcdefghijklmnopqrstuvwxyz",
-      labels = "asdfghjklqwertyuiopzxcvbnm",
+      labels = "asdfghjklqwertzuiopyxcvbnm",
       search = {
-        -- search/jump in all windows
         multi_window = true,
-        -- search direction
         forward = true,
-        -- when `false`, find only matches in the given direction
         wrap = true,
-        ---@type Flash.Pattern.Mode
-        -- Each mode will take ignorecase and smartcase into account.
-        -- * exact: exact match
-        -- * search: regular search
-        -- * fuzzy: fuzzy search
-        -- * fun(str): custom function that returns a pattern
-        --   For example, to only match at the beginning of a word:
-        --   mode = function(str)
-        --     return "\\<" .. str
-        --   end,
         mode = "exact",
-        -- behave like `incsearch`
         incremental = false,
-        -- Excluded filetypes and custom window filters
-        ---@type (string|fun(win:window))[]
         exclude = {
           "notify",
           "cmp_menu",
           "noice",
           "flash_prompt",
           function(win)
-            -- exclude non-focusable windows
             return not vim.api.nvim_win_get_config(win).focusable
           end,
         },
-        -- Optional trigger character that needs to be typed before
-        -- a jump label can be used. It's NOT recommended to set this,
-        -- unless you know what you're doing
         trigger = "",
-        -- max pattern length. If the pattern length is equal to this
-        -- labels will no longer be skipped. When it exceeds this length
-        -- it will either end in a jump or terminate the search
         max_length = false, ---@type number|false
       },
       jump = {
-        -- save location in the jumplist
         jumplist = true,
-        -- jump position
         pos = "start", ---@type "start" | "end" | "range"
-        -- add pattern to search history
         history = false,
-        -- add pattern to search register
         register = false,
-        -- clear highlight after jump
         nohlsearch = false,
-        -- automatically jump when there is only one match
         autojump = false,
-        -- You can force inclusive/exclusive jumps by setting the
-        -- `inclusive` option. By default it will be automatically
-        -- set based on the mode.
         inclusive = nil, ---@type boolean?
-        -- jump position offset. Not used for range jumps.
-        -- 0: default
-        -- 1: when pos == "end" and pos < current position
         offset = nil, ---@type number
       },
       label = {
-        -- allow uppercase labels
         uppercase = true,
-        -- add any labels with the correct case here, that you want to exclude
         exclude = "",
-        -- add a label for the first match in the current window.
-        -- you can always jump to the first match with `<CR>`
         current = true,
-        -- show the label after the match
         after = true, ---@type boolean|number[]
-        -- show the label before the match
         before = false, ---@type boolean|number[]
-        -- position of the label extmark
         style = "overlay", ---@type "eol" | "overlay" | "right_align" | "inline"
-        -- flash tries to re-use labels that were already assigned to a position,
-        -- when typing more characters. By default only lower-case labels are re-used.
         reuse = "lowercase", ---@type "lowercase" | "all" | "none"
-        -- for the current window, label targets closer to the cursor first
         distance = true,
-        -- minimum pattern length to show labels
-        -- Ignored for custom labelers.
         min_pattern_length = 0,
-        -- Enable this to use rainbow colors to highlight labels
-        -- Can be useful for visualizing Treesitter ranges.
         rainbow = {
           enabled = true,
           -- number between 1 and 9
           shade = 5,
         },
-        -- With `format`, you can change how the label is rendered.
-        -- Should return a list of `[text, highlight]` tuples.
-        ---@class Flash.Format
-        ---@field state Flash.State
-        ---@field match Flash.Match
-        ---@field hl_group string
-        ---@field after boolean
-        ---@type fun(opts:Flash.Format): string[][]
         format = function(opts)
           return { { opts.match.label, opts.hl_group } }
         end,
       },
       highlight = {
-        -- show a backdrop with hl FlashBackdrop
         backdrop = true,
-        -- Highlight the search matches
         matches = true,
-        -- extmark priority
         priority = 5000,
         groups = {
           match = "FlashMatch",
@@ -155,88 +94,44 @@ return {
           label = "FlashLabel",
         },
       },
-      -- action to perform when picking a label.
-      -- defaults to the jumping logic depending on the mode.
-      ---@type fun(match:Flash.Match, state:Flash.State)|nil
       action = nil,
-      -- initial pattern to use when opening flash
       pattern = "",
-      -- When `true`, flash will try to continue the last search
       continue = false,
-      -- Set config to a function to dynamically change the config
       config = nil, ---@type fun(opts:Flash.Config)|nil
-      -- You can override the default options for a specific mode.
-      -- Use it with `require("flash").jump({mode = "forward"})`
-      ---@type table<string, Flash.Config>
       modes = {
-        -- options used when flash is activated through
-        -- a regular search with `/` or `?`
         search = {
-          -- when `true`, flash will be activated during regular search by default.
-          -- You can always toggle when searching with `require("flash").toggle()`
           enabled = true,
           highlight = { backdrop = false },
           jump = { history = true, register = true, nohlsearch = true },
-          search = {
-            -- `forward` will be automatically set to the search direction
-            -- `mode` is always set to `search`
-            -- `incremental` is set to `true` when `incsearch` is enabled
-          },
+          search = {},
         },
-        -- options used when flash is activated through
-        -- `f`, `F`, `t`, `T`, `;` and `,` motions
         char = {
           enabled = true,
-          -- dynamic configuration for ftFT motions
           config = function(opts)
-            -- autohide flash when in operator-pending mode
             opts.autohide = opts.autohide or (vim.fn.mode(true):find("no") and vim.v.operator == "y")
 
-            -- disable jump labels when not enabled, when using a count,
-            -- or when recording/executing registers
             opts.jump_labels = opts.jump_labels
               and vim.v.count == 0
               and vim.fn.reg_executing() == ""
               and vim.fn.reg_recording() == ""
-
-            -- Show jump labels only in operator-pending mode
-            -- opts.jump_labels = vim.v.count == 0 and vim.fn.mode(true):find("o")
           end,
-          -- hide after jump when not using jump labels
           autohide = false,
-          -- show jump labels
           jump_labels = false,
-          -- set to `false` to use the current line only
           multi_line = true,
-          -- When using jump labels, don't use these keys
-          -- This allows using those keys directly after the motion
           label = { exclude = "hjkliardc" },
-          -- by default all keymaps are enabled, but you can disable some of them,
-          -- by removing them from the list.
-          -- If you rather use another key, you can map them
-          -- to something else, e.g., { [";"] = "L", [","] = H }
           keys = { "f", "F", "t", "T", ";", "," },
-          ---@alias Flash.CharActions table<string, "next" | "prev" | "right" | "left">
-          -- The direction for `prev` and `next` is determined by the motion.
-          -- `left` and `right` are always left and right.
           char_actions = function(motion)
             return {
               [";"] = "next", -- set to `right` to always go right
               [","] = "prev", -- set to `left` to always go left
-              -- clever-f style
               [motion:lower()] = "next",
               [motion:upper()] = "prev",
-              -- jump2d style: same case goes next, opposite case goes prev
-              -- [motion] = "next",
-              -- [motion:match("%l") and motion:upper() or motion:lower()] = "prev",
             }
           end,
           search = { wrap = false },
           highlight = { backdrop = true },
           jump = { register = false },
         },
-        -- options used for treesitter selections
-        -- `require("flash").treesitter()`
         treesitter = {
           labels = "abcdefghijklmnopqrstuvwxyz",
           jump = { pos = "range" },
@@ -253,13 +148,10 @@ return {
           remote_op = { restore = true },
           label = { before = true, after = true, style = "inline" },
         },
-        -- options used for remote flash
         remote = {
           remote_op = { restore = true, motion = true },
         },
       },
-      -- options for the floating window that shows the prompt,
-      -- for regular jumps
       prompt = {
         enabled = true,
         prefix = { { "⚡", "FlashPromptIcon" } },
@@ -272,15 +164,8 @@ return {
           zindex = 1000,
         },
       },
-      -- options for remote operator pending mode
       remote_op = {
-        -- restore window views and cursor position
-        -- after doing a remote operation
         restore = false,
-        -- For `jump.pos = "range"`, this setting is ignored.
-        -- `true`: always enter a new motion when doing a remote operation
-        -- `false`: use the window's cursor position and jump target
-        -- `nil`: act as `true` for remote windows, `false` for the current window
         motion = false,
       },
     },
@@ -297,8 +182,6 @@ return {
     event = "VeryLazy",
     dependencies = "nvim-treesitter/nvim-treesitter",
     config = true,
-    -- Uncomment next line if you want to follow only stable versions
-    -- version = "*"
   },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -308,17 +191,9 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
         ensure_installed = "all",
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
         auto_install = true,
-
-        -- List of parsers to ignore installing (or "all")
         ignore_install = {
           "javascript",
           "norg",
@@ -327,18 +202,9 @@ return {
           "bicep",
         },
 
-        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
         highlight = {
           enable = true,
 
-          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-          -- the name of the parser)
-          -- list of language that will be disabled
-          -- disable = { "c", "rust" },
-          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
           disable = function(lang, buf)
             local max_filesize = 5 * 1024 * 1024 -- 5 MB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -373,8 +239,6 @@ return {
         multiline_threshold = 20, -- Maximum number of lines to show for a single context
         trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
         mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
         separator = "-",
         zindex = 20, -- The Z-index of the context window
         on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
@@ -394,64 +258,34 @@ return {
         },
         -- delay: delay in milliseconds
         delay = 100,
-        -- filetype_overrides: filetype specific overrides.
-        -- The keys are strings to represent the filetype while the values are tables that
-        -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
         filetype_overrides = {},
-        -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
         filetypes_denylist = {
           "dirbuf",
           "dirvish",
           "fugitive",
         },
-        -- filetypes_allowlist: filetypes to illuminate, this is overridden by filetypes_denylist
-        -- You must set filetypes_denylist = {} to override the defaults to allow filetypes_allowlist to take effect
         filetypes_allowlist = {},
-        -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
-        -- See `:help mode()` for possible values
         modes_denylist = {},
-        -- modes_allowlist: modes to illuminate, this is overridden by modes_denylist
-        -- See `:help mode()` for possible values
         modes_allowlist = {},
-        -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
-        -- Only applies to the 'regex' provider
-        -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
         providers_regex_syntax_denylist = {},
-        -- providers_regex_syntax_allowlist: syntax to illuminate, this is overridden by providers_regex_syntax_denylist
-        -- Only applies to the 'regex' provider
-        -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
         providers_regex_syntax_allowlist = {},
-        -- under_cursor: whether or not to illuminate under the cursor
         under_cursor = true,
-        -- large_file_cutoff: number of lines at which to use large_file_config
-        -- The `under_cursor` option is disabled when this cutoff is hit
         large_file_cutoff = nil,
-        -- large_file_config: config to use for large files (based on large_file_cutoff).
-        -- Supports the same keys passed to .configure
-        -- If nil, vim-illuminate will be disabled for large files.
         large_file_overrides = nil,
-        -- min_count_to_highlight: minimum number of matches required to perform highlighting
         min_count_to_highlight = 1,
-        -- should_enable: a callback that overrides all other settings to
-        -- enable/disable illumination. This will be called a lot so don't do
-        -- anything expensive in it.
         should_enable = function(bufnr)
           return true
         end,
-        -- case_insensitive_regex: sets regex case sensitivity
         case_insensitive_regex = false,
       })
 
-      -- Add external parsers
       local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
       parser_config.plantuml = {
         install_info = {
-          url = "https://github.com/Decodetalkers/tree_sitter_plantuml.git", -- local path or git repo
+          url = "https://github.com/lyndsysimon/tree-sitter-plantuml",
           files = { "src/parser.c" },
-          branch = "gh-pages",
-          generate_requires_npm = false,
-          requires_generate_from_grammar = true,
+          revision = "main",
         },
         filetype = "plantuml",
       }
@@ -464,7 +298,6 @@ return {
       -- This module contains a number of default definitions
       local rainbow_delimiters = require("rainbow-delimiters")
 
-      ---@type rainbow_delimiters.config
       vim.g.rainbow_delimiters = {
         strategy = {
           [""] = rainbow_delimiters.strategy["global"],
