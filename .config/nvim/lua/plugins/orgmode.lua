d7fbffe099c390e8d16a7c92177206c9534f5960
Dominikus Gierlach <dominikus.gierlach@man.eu>
2024-10-21 10:22:22 +0200

    change default note file location

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -7,7 +7,7 @@ return {
       -- Setup orgmode
       require("orgmode").setup({
         org_agenda_files = { "~/Sync/org/**/*", "~/org/**/*" },
-        org_default_notes_file = "~/Sync/org/refile.org",
+        org_default_notes_file = "~/Sync/org/notes.org",
         org_capture_templates = {
           t = {
             description = "Task",
