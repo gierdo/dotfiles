cc05a67331e0fce6f87d2b3c6d48c19b8f777706
Dominikus Gierlach <dominikus.gierlach@man.eu>
2024-11-14 16:38:03 +0100

    remove lsp-notify

--- .config/nvim/lua/plugins/lsp-behaviour.lua
+++ .config/nvim/lua/plugins/lsp-behaviour.lua
@@ -206,15 +206,4 @@ return {
       },
     },
   },
-  {
-    "mrded/nvim-lsp-notify",
-    config = function()
-      require("lsp-notify").setup({
-        notify = require("notify"),
-      })
-    end,
-    dependencies = {
-      "rcarriga/nvim-notify",
-    },
-  },
 }
