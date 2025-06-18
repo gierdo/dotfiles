ec4d8bbf8ad86224cac27b6c8cfb04a8eaddc8b5
Dominikus Gierlach <dominik.gierlach@gmail.com>
2025-06-18 16:46:02 +0200

    fix font-size scaling in wezterm with tiling

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,6 +15,7 @@ config.font = wezterm.font_with_fallback({
 })
 
 config.font_size = 13.0
+config.adjust_window_size_when_changing_font_size = false
 config.warn_about_missing_glyphs = false
 
 return config
