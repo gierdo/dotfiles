6754cda06d77654afe162fb3d4a315afd627ee02
Dominikus Gierlach <dominik.gierlach@gmail.com>
2025-08-02 16:17:10 +0200

    add noice

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -148,5 +148,28 @@ return {
     "stevearc/dressing.nvim",
     config = true,
   },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("noice").setup({
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+          lsp_doc_border = true,
+        },
+      })
+    end,
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
   "rcarriga/nvim-notify",
 }
