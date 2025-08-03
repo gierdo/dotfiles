824cbe3917c9de58b0214bfae3b77572b9c30f39
Dominikus Gierlach <dominik.gierlach@gmail.com>
2025-08-03 13:35:56 +0200

    fix gpu support in local llama container

--- gists/install-llama-models.sh
+++ gists/install-llama-models.sh
@@ -16,5 +16,6 @@ function install_model() {
   fi
 }
 
+install_model https://huggingface.co/bartowski/Ministral-8B-Instruct-2410-GGUF/resolve/main/Ministral-8B-Instruct-2410-Q4_K_M.gguf Ministral-8B-Instruct-2410-Q4_K_M.gguf
 install_model https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF/resolve/main/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf
 install_model https://huggingface.co/Qwen/Qwen2.5-Coder-7B-Instruct-GGUF/resolve/main/qwen2.5-coder-7b-instruct-q4_k_m.gguf qwen2.5-coder-7b-instruct-q4_k_m.gguf
