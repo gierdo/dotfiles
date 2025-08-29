d293ade8d8237af43fa6a2d7c88641d2ce872d7a
Dominikus Gierlach <dominik.gierlach@gmail.com>
2025-08-29 09:36:16 +0200

    add lensline plugin

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -3,7 +3,7 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup({ current_line_blame = true })
-    end
+    end,
   },
   {
     "tpope/vim-fugitive",
@@ -43,8 +43,8 @@ return {
   {
     "NeogitOrg/neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
       "nvim-telescope/telescope.nvim", -- optional
     },
     config = true,
@@ -52,5 +52,51 @@ return {
   {
     "akinsho/git-conflict.nvim",
     config = true,
+  },
+  {
+    "oribarilan/lensline.nvim",
+    brnach = "release/1.x",
+    event = "LspAttach",
+    config = function()
+      require("lensline").setup({
+        providers = {
+          {
+            name = "references",
+            enabled = true,
+            quiet_lsp = true,
+          },
+          {
+            name = "last_author",
+            enabled = true,
+            cache_max_files = 50,
+          },
+
+          {
+            name = "diagnostics",
+            enabled = true,
+            min_level = "WARN",
+          },
+          {
+            name = "complexity",
+            enabled = true,
+            min_level = "L",
+          },
+        },
+        style = {
+          separator = " • ",
+          highlight = "Comment",
+          prefix = "┃ ",
+          use_nerdfont = true,
+        },
+        limits = {
+          exclude = {},
+          exclude_gitignored = true,
+          max_lines = 1000,
+          max_lenses = 70,
+        },
+        debounce_ms = 500,
+        debug_mode = false,
+      })
+    end,
   },
 }
