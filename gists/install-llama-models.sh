a7872f2b11649cf32edf8003a550e1d70a225a4a
Dominikus Gierlach <dominik.gierlach@gmail.com>
2025-10-18 10:23:02 +0200

    use tool-use optimized llama 3

--- gists/install-llama-models.sh
+++ gists/install-llama-models.sh
@@ -16,6 +16,5 @@ function install_model() {
   fi
 }
 
-install_model https://huggingface.co/bartowski/Ministral-8B-Instruct-2410-GGUF/resolve/main/Ministral-8B-Instruct-2410-Q4_K_M.gguf Ministral-8B-Instruct-2410-Q4_K_M.gguf
 install_model https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF/resolve/main/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf
-install_model https://huggingface.co/Qwen/Qwen2.5-Coder-7B-Instruct-GGUF/resolve/main/qwen2.5-coder-7b-instruct-q4_k_m.gguf qwen2.5-coder-7b-instruct-q4_k_m.gguf
+install_model https://huggingface.co/bartowski/Llama-3-Groq-8B-Tool-Use-GGUF/resolve/main/Llama-3-Groq-8B-Tool-Use-Q4_K_M.gguf Llama-3-Groq-8B-Tool-Use-Q4_K_M.gguf
