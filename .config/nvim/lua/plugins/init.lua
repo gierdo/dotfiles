d554fb7a2af103e99949a4956a99748f2366171e
Dominikus Gierlach <dominik.gierlach@gmail.com>
2025-11-19 21:11:38 +0100

    add codesnap

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,6 +1,22 @@
 return {
   { "nvim-lua/plenary.nvim", lazy = true }, -- lua base library
   {
+    "mistricky/codesnap.nvim",
+    build = "make",
+    config = function()
+      require("codesnap").setup({
+        save_path = os.getenv("XDG_PICTURES_DIR") or (os.getenv("HOME") .. "/Pictures"),
+        code_font_family = "UbuntuMono NerdFont",
+        watermark_font_family = "UbuntuMono NerdFont",
+        bg_x_padding = 122,
+        bg_y_padding = 82,
+        has_line_number = true,
+        bg_theme = "sea",
+        watermark = "https://gierdo.astounding.technology",
+      })
+    end,
+  },
+  {
     "folke/which-key.nvim",
     event = "VeryLazy",
     config = function()
