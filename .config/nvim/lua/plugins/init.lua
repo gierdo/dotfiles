fb78aa899466ebb4469df73c50ac34c2894c21e3
Dominikus Gierlach <dominik.gierlach@gmail.com>
2026-01-25 13:39:22 +0100

    add hexview

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -33,10 +33,9 @@ return {
   },
   "tpope/vim-sleuth",
   {
-    "RaafatTurki/hex.nvim",
-    event = "VeryLazy",
+    "DamianVCechov/hexview.nvim",
     config = function()
-      require("hex").setup()
+      require("hexview").setup()
     end,
   },
   {
