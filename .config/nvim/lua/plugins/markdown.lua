0c40374cef85e41a8865c9777520496a960dfdbb
Dominikus Gierlach <dominik.gierlach@gmail.com>
2026-06-25 17:32:46 +0200

    simplify markdown setup

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,140 +1,59 @@
 return {
   {
-    "obsidian-nvim/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
+    "yousefhadder/markdown-plus.nvim",
     ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "saghen/blink.cmp",
+    opts = {
+      enabled = true,
+      features = {
+        list_management = true,
+        text_formatting = true,
+        thematic_break = true,
+        headers_toc = true,
+        links = true,
+        images = true,
+        quotes = true,
+        callouts = true,
+        code_block = true,
+        html_block_awareness = true,
+        table = true,
+        footnotes = true,
+      },
+      footnotes = {
+        section_header = "Footnotes",
+        confirm_delete = true,
+      },
+      keymaps = {
+        enabled = true,
+      },
+      toc = {
+        initial_depth = 2,
+      },
+      callouts = {
+        default_type = "NOTE",
+        custom_types = {},
+      },
+      table = {
+        auto_format = true,
+        default_alignment = "left",
+        confirm_destructive = true,
+        keymaps = {
+          enabled = true,
+          prefix = "<localleader>t",
+          insert_mode_navigation = true,
+        },
+      },
+      filetypes = { "markdown" },
     },
-
-    config = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "markdown",
-        command = "setlocal conceallevel=2",
-      })
-
-      require("obsidian").setup({
-        legacy_commands = false,
-        footer = { enabled = false },
-        workspaces = {
-          {
-            name = "notes",
-            path = "~/Sync/notes",
-          },
-          {
-            name = "workspace_notes",
-            path = "~/workspace/workspace_notes",
-          },
-          {
-            name = "no-vault",
-            path = function()
-              -- If we are in diff mode, return the current working directory instead of the tmp directory to the diff buffer
-              if vim.wo.diff then
-                return vim.fn.getcwd()
-              end
-              return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
-            end,
-            overrides = { ---@diagnostic disable-line: missing-fields
-              notes_subdir = vim.NIL, ---@diagnostic disable-line: assign-type-mismatch
-              new_notes_location = "current_dir",
-              templates = { ---@diagnostic disable-line: missing-fields
-                folder = vim.NIL, ---@diagnostic disable-line: assign-type-mismatch
-              },
-              frontmatter = { enabled = false },
-            },
-          },
-        },
-
-        -- Either 'wiki' or 'markdown'.
-        link = {
-          style = "markdown",
-        },
-
-        frontmatter = { enabled = false },
-
-        -- Optional, for templates (see below).
-        templates = { ---@diagnostic disable-line: missing-fields
-          folder = "templates",
-          date_format = "%Y-%m-%d",
-          time_format = "%H:%M",
-          -- A map for custom variables, the key should be the variable and the value a function
-          substitutions = {},
-        },
-
-        picker = { ---@diagnostic disable-line: missing-fields
-          -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-          name = "telescope.nvim",
-          -- Optional, configure key mappings for the picker. These are the defaults.
-          -- Not all pickers support all mappings.
-          note_mappings = { ---@diagnostic disable-line: missing-fields
-            -- Create a new note from your query.
-            new = "<C-x>",
-            -- Insert a link to the selected note.
-            insert_link = "<C-l>",
-          },
-          tag_mappings = { ---@diagnostic disable-line: missing-fields
-            -- Add tag(s) to current note.
-            tag_note = "<C-x>",
-            -- Insert a tag at the current location.
-            insert_tag = "<C-l>",
-          },
-        },
-
-        search = { max_lines = 1000, sort_by = "modified", sort_reversed = true },
-
-        -- Optional, determines how certain commands open notes. The valid options are:
-        -- 1. "current" (the default) - to always open in the current window
-        -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
-        -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
-        open_notes_in = "current",
-
-        -- Optional, define your own callbacks to further customize behavior.
-        callbacks = { ---@diagnostic disable-line: missing-fields
-          -- Runs at the end of `require("obsidian").setup()`.
-          ---@param client obsidian.Client
-          post_setup = function(client) end,
-
-          -- Runs anytime you enter the buffer for a note.
-          ---@param client obsidian.Client
-          ---@param note obsidian.Note
-          enter_note = function(client, note) end,
-
-          -- Runs anytime you leave the buffer for a note.
-          ---@param client obsidian.Client
-          ---@param note obsidian.Note
-          leave_note = function(client, note) end,
-
-          -- Runs right before writing the buffer for a note.
-          ---@param client obsidian.Client
-          ---@param note obsidian.Note
-          pre_write_note = function(client, note) end,
-
-          -- Runs anytime the workspace is set/changed.
-          ---@param client obsidian.Client
-          ---@param workspace obsidian.Workspace
-          post_set_workspace = function(client, workspace) end,
-        },
-        ui = {
-          enable = false, -- set to false to disable all additional syntax features
-        },
-      })
-    end,
   },
   {
-    "Zeioth/markmap.nvim",
-    build = "yarn global add markmap-cli",
-    cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
-    opts = {
-      hide_toolbar = false, -- (default)
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
     },
-    config = function(_, opts)
-      require("markmap").setup(opts)
-    end,
-    keys = {
-      { "<leader>mm", "<cmd>MarkmapOpen<cr>", desc = "Open Markmap" },
-    },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
   },
   {
     "iamcco/markdown-preview.nvim",
@@ -316,15 +235,6 @@ return {
     },
     keys = {
       { "<leader>mp", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-    },
-  },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {
-      pipe_table = {
-        enabled = false,
-      },
     },
   },
 }
