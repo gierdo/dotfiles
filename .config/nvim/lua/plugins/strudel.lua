5edd734e71469509660286bb60c1eee88dece0de
Dominikus Gierlach <dominik.gierlach@gmail.com>
2026-07-30 10:38:23 +0200

    pin node version for strudel plugin

--- .config/nvim/lua/plugins/strudel.lua
+++ .config/nvim/lua/plugins/strudel.lua
@@ -1,7 +1,15 @@
 return {
   {
     "gruvw/strudel.nvim",
-    build = "npm ci",
+    -- node 22: avoids yargs ESM/CJS breakage on node 26+
+    build = table.concat({
+      [[printf '[tools]\nnode = "22"\n' > .mise.toml]],
+      "mise trust",
+      "mise install",
+      "mise exec -- npm ci",
+      [[sed -i 's|local cmd = "node "|local cmd = "mise exec -C " .. plugin_root .. " -- node "|' lua/strudel/init.lua]],
+      "true",
+    }, " && "),
     config = function()
       require("strudel").setup()
     end,
