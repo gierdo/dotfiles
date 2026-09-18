00e4463b160dc96eee6799f7363db1bf6c09c06b
Dominikus Gierlach <dominik.gierlach@gmail.com>
2026-09-18 10:16:15 +0200

    chore: update deps

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -99,14 +99,6 @@ return {
     },
   },
   {
-    "preservim/vim-markdown",
-    config = function()
-      vim.g.vim_markdown_folding_disabled = 1
-      vim.g.vim_markdown_auto_insert_bullets = 0
-      vim.g.vim_markdown_new_list_item_indent = 0
-    end,
-  },
-  {
     "weirongxu/plantuml-previewer.vim",
     cmd = {
       "PlantumlOpen",
