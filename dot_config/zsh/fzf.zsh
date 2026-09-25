0abc8bfa3a07a887d801122aee3cb8eaf65553f9
Dominikus Gierlach <dominik.gierlach@gmail.com>
2026-09-25 13:57:13 +0200

    sanitized zsh config

---
+++ dot_config/zsh/fzf.zsh
@@ -0,0 +1,7 @@
+_fzf_compgen_path() {
+  rg --files . "$1"
+}
+
+_fzf_compgen_dir() {
+  fd --type d --hidden . "$1"
+}
--- zsh-plugins/custom-fzf/fzf.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-_fzf_compgen_path() {
-    rg --files . "$1"
-}
-
-_fzf_compgen_dir() {
-   fd --type d --hidden . "$1"
-}
